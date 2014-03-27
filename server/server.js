176ed500983d83ed06ec748a2a2187121046f59b
Devin Tuchsen <devint1@icloud.com>
2014-03-26 19:21:43 -0500

    Updated server code, moved lobby.psd

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
-var http = require("http");
+var io = require('socket.io').listen(56644);
+var userNames = new Array();
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(56644);
+io.sockets.on('connection', function(socket) {
+	var myIndex;
+	socket.emit('userLobby', userNames);
+	socket.on('userName', function(data) {
+		console.log(data);
+		socket.broadcast.emit('newUser', data);
+		for(var x = 0; x < 4; ++x) {
+		if(userNames[x] == null) {
+			userNames[x] = data;
+			break;
+		}
+	}
+		myIndex = userNames.length - 1;
+	});
+	socket.on('disconnect', function(data) {
+		console.log("DISCONNECTED!!!!!");
+		console.log(data);
+		delete userNames[myIndex];
+		socket.broadcast.emit('userDisconnect', myIndex);
+	});
+});
 
 console.log("Server started.");
